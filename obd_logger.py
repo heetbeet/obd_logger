37ec9439ccccbce0442f6f4f2bdd778b27434bfd
Your Name <you@example.com>
2020-12-25 19:08:15 +0200

    add things

--- obd_logger.py
+++ obd_logger.py
@@ -10,7 +10,7 @@ print("**********************************")
 print("Starting ", str(datetime.datetime.now()))
 print("**********************************", flush=True)
 
-# redirect odb logger warnings and logger errors to actual python errors
+# Redirect odb logger warnings and logger errors to actual python errors
 class LevelRaiser(logging.Handler):
 
     def emit(self, record: logging.LogRecord):
@@ -22,36 +22,43 @@ for key, value in obd.__dict__.items():
         library_root_logger = logging.getLogger(value.__name__)
         library_root_logger.addHandler(LevelRaiser())
 
+try:
+    # auto-connects to USB or RF port
+    connection = obd.OBD() 
 
-# auto-connects to USB or RF port
-connection = obd.OBD() 
-
-# find car's compatable commands
-supported_headers = [i for i in dir(obd.commands) if isinstance(getattr(obd.commands, i), obd.OBDCommand) and
-                                                    connection.supports(getattr(obd.commands, i))]
+    # find car's compatable commands
+    supported_headers = [i for i in dir(obd.commands) if isinstance(getattr(obd.commands, i), obd.OBDCommand) and
+                                                        connection.supports(getattr(obd.commands, i))]
 
 
-# read car's vitals line by line and write to csv
-localtime = time.localtime(time.time())
-os.mkdirs("/home/pi/logs", exist_ok=True)
-filename = "/home/pi/logs/carlogger-"+str(localtime[0])+"-"+str(localtime[1])+"-"+str(localtime[2])+"-"+str(localtime[3])+"-"+str(localtime[4])+"-"+str(localtime[5])+".csv"
-
-with open(filename, "a") as fw:
-    fw.write(",".join(['TIME']+supported_headers) + "\n")
-
-while(True):
-    vals = [str(datetime.datetime.now())]
-    errs = 0
-    for i in supported_headers:
-        try:
-            vals.append(str(connection.query(getattr(obd.commands, i)).value))
-        except Exception as e:
-            vals.append(str(e))
-            errs += 1
+    # read car's vitals line by line and write to csv
+    localtime = time.localtime(time.time())
+    os.mkdirs("/home/pi/logs", exist_ok=True)
+    filename = "/home/pi/logs/carlogger-"+str(localtime[0])+"-"+str(localtime[1])+"-"+str(localtime[2])+"-"+str(localtime[3])+"-"+str(localtime[4])+"-"+str(localtime[5])+".csv"
 
     with open(filename, "a") as fw:
-        fw.write('"' + ('","'.join([i.replace('"', "'") for i in vals])) + '"\n')
+        fw.write(",".join(['TIME']+supported_headers) + "\n")
 
-    # if half of the headers end in errors, then a restart error is probably needed
-    if int(len(supported_headers)/2) <= errs:
-        raise ValueError("Too many errors during readings")
+    while(True):
+        vals = [str(datetime.datetime.now())]
+        errs = 0
+        for i in supported_headers:
+            try:
+                vals.append(str(connection.query(getattr(obd.commands, i)).value))
+            except Exception as e:
+                vals.append(str(e))
+                errs += 1
+
+        with open(filename, "a") as fw:
+            fw.write('"' + ('","'.join([i.replace('"', "'") for i in vals])) + '"\n')
+
+        # if half of the headers end in errors, then a restart error is probably needed
+        if int(len(supported_headers)/2) <= errs:
+            raise ValueError("Too many errors during readings")
+
+finally:
+    # Try to keep connection closed
+    try:
+        connection.close()
+    except:
+        pass
